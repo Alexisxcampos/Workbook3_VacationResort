1a94c7acadf9cbada22748c7b4107e58a911f12c
A. Xochitl Campos <55463563+Alexisxcampos@users.noreply.github.com>
2023-10-30 23:56:58 -0400

    Add files via upload
    changes

---
+++ overnightplanner.js
@@ -0,0 +1,24 @@
+// I neeeeeeeeeed .. 
+
+//For the button to do something when it's clicked..........
+
+function DisplayCost () {
+    const TheWholePoint = document.getElementById("outPutDiv");
+    outputdiv.innerHTML = `Original Room Cost: ${} <br> Discount: ${} 
+    <br> Discounted Cost: ${} <br> Tax: ${} <br> Total Cost of Stay: ${}`;
+}
+
+
+//Original Room Cost
+
+
+//Discount
+
+
+//Discounted Room Cost
+
+
+//Tax 12%
+
+
+//Total Cost of stay
